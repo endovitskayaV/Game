54ce4055347121fbe929d2899791ff87b73e65c0
Valentina Endovitskaya <valyalark@hotmail.com>
2016-09-28 19:12:14 +0300

    created second form layoyt

---
+++ Game/FormFood.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+   public partial class FormFood: Form
+    {
+        private TextBox textBox1;
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Tb_Mouse_Click);
+            // 
+            // FormFood
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FormFood";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Tb_Mouse_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+
+}
